b57730a9dcf572d3cc4f522e18342cd9f7a07208
dlin21 <90690772+dlin21@users.noreply.github.com>
2021-12-18 13:20:40 -0500

    Update pipe_networking.c

--- pipe_networking.c
+++ pipe_networking.c
@@ -21,6 +21,7 @@ int server_handshake(int *to_client) {
     printf("error: %s\n", strerror(errno));
     return 0;
   }
+  
   printf("reading private FIFO name...\n");
   // client message
   char cm[HANDSHAKE_BUFFER_SIZE];
@@ -45,11 +46,16 @@ int server_handshake(int *to_client) {
     printf("error: %s\n", strerror(errno));
     return 0;
   }
+  
   // server receives final response from client
   printf("receiving response from client...\n");
   err = read(from_client, cm, sizeof(cm));
   if(err == -1){
     printf("error: %s\n", strerror(errno));
+    return 0;
+  }
+  if(strcmp(cm, ACK) != 0){
+    printf("handshake failed.../n");
     return 0;
   }
   
@@ -66,13 +72,11 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  int from_server;
-  char sp[1000];
   
   printf("creating private FIFO...\n");
-  int pid = getpid();
-  sscanf(sp, "%d", &pid);
-  mkfifo(sp, 0644);
+  char pid[1000];
+  sprintf(pid, "&d", getpid());
+  mkfifo(pid, 0644);
   
   printf("opening wkp...\n");
   *to_server = open(WKP, O_WRONLY);
@@ -80,27 +84,30 @@ int client_handshake(int *to_server) {
     printf("error: %s\n", strerror(errno));
     return 0;
   }
+  
   printf("sending private FIFO name...\n");
   int err = write(*to_server, pid, HANDSHAKE_BUFFER_SIZE);
   if(err == -1){
     printf("error: %s\n", strerror(errno));
     return 0;
   }
+  
   printf("receiving messeage from server...\n");
   int from_server = open(pid, O_RDONLY);
   if(from_server == -1){
     printf("error: %s\n", strerror(errno));
     return 0;
   }
+  
   printf("reading server message...\n");
   char cm[HANDSHAKE_BUFFER_SIZE];
   err = read(from_server, cm, HANDSHAKE_BUFFER_SIZE);
   if(err == -1){
     printf("error: %s\n", strerror(errno));
     return 0;
-  }
-  
+  }  
   remove(pid);
+  
   printf("sending response to server...\n");
   err = write(*to_server, ACK, sizeof(ACK));
   if(err == -1){
